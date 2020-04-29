98d1fbe116011623999ff77ef27de231b8683abe
Malabi Poddar <malabi.poddar1@t-mobile.com>
2020-04-29 15:02:39 +0530

    Task1B: AccessibilityCahngesAdded

--- apps/stocks/src/environments/environment.ts
+++ apps/stocks/src/environments/environment.ts
@@ -6,7 +6,7 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: false,
-  apiKey: '',
+  apiKey: 'Tpk_ed7d206f00d841a8bfa2eae3778bb615',
   apiURL: 'https://sandbox.iexapis.com'
 };
 
